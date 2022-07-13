ec507de0b029fec7e5b074375458c20e9e3c7925
Lucca Nunes <luccanrib@gmail.com>
2022-07-13 13:46:53 -0300

    adicionando redirecionamento no login normal(anteriormente so estava no login do google

--- main/views.py
+++ main/views.py
@@ -27,7 +27,7 @@ def login_request(request):
 			if user is not None:
 				login(request, user)
 				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("main:homepage")
+				return redirect("main:timeline_page")
 			else:
 				messages.error(request,"Invalid username or password.")
 		else:
