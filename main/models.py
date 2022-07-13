d35cc246cee287feef24d5e0cfee364878745e88
Gabriel Santos <gab.tec97@gmail.com>
2022-07-13 16:09:44 -0300

    Cria os models de like e tweet além de apontar para o User obtido pelo auth do Django

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
+from django.utils import timezone
 from django.db import models
+from .forms import User
 
 # Create your models here.
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tweets")
+    content = models.CharField(max_length=140)
+    created_on = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="like", null=True, blank=True)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE, related_name="like")
+    count = models.BooleanField(default=False)
+
+
+
+    def __str__(self):
+        text = f'{self.tweet} | {self.count}'
+        return text
