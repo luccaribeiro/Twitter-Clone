fc596647b7ddce4605d97b995f7b92bd06f446e8
Gabriel Santos <gab.tec97@gmail.com>
2022-07-17 15:20:37 -0300

    Finaliza a criação da tabela Profile, resolve as inconsistências nos querysets que isso trouxe e também exibe certos dados que até então vinham da User

--- mysite/settings.py
+++ mysite/settings.py
@@ -165,6 +165,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static/"
 ]
 
+# MEDIA
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- timeline/urls.py
+++ timeline/urls.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("timeline/", views.principal, name='timeline_page'),
     path("perfil/<str:username>/", views.perfil, name='perfil')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
